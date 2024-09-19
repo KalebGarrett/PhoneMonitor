ec48429919d4cc50f85795a9af1d7a6e64235c20
Kaleb Garrett <kalebagarrett@gmail.com>
2024-09-19 15:47:59 -0400

    Changed file properties for toast image

--- PhoneMonitor.App/Services/ToastService.cs
+++ PhoneMonitor.App/Services/ToastService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.IO;
+using System.Reflection;
 using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace PhoneMonitor.App.Services
@@ -10,14 +11,14 @@ namespace PhoneMonitor.App.Services
         {
             var random = new Random();
             var conversationId = random.Next(1, 10000);
-
-            var imgPath = @"D:\repos\PhoneMonitor\PhoneMonitor.App\Images\finger-shake-judge-judy.gif";
-      
+            
+            var imgPathUri = Path.GetFullPath(@"Images\finger-shake-judge-judy.gif");
+            
             var toast = new ToastContentBuilder();
             toast.AddArgument("action", "viewConversation");
             toast.AddArgument("conversationId", conversationId);
             toast.AddText("Get off your phone! You are working.");
-            toast.AddInlineImage(new Uri(imgPath));
+            toast.AddInlineImage(new Uri(imgPathUri));
             toast.Show();
         }
     }
